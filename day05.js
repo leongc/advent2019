d246524af7c556e600f5ca9b5fe99aa8f4f7adc7
Cheng Leong <leongc@alumni.rice.edu>
2019-12-08 01:58:19 -0600

    Update day05.js

--- day05.js
+++ day05.js
@@ -47,7 +47,8 @@ Finally, the program will output a diagnostic code and immediately halt. This fi
 
 After providing 1 to the only input instruction and passing all the tests, what diagnostic code does the program produce?
 */
-function run(program, inputValue) {
+function run(original, inputValue) {
+  var program = original.slice();
   let pc = 0;
   while (true) {
     let opcode = program[pc] % 100;
@@ -95,3 +96,146 @@ console.assert(run([1101,100,-1,4,0]).join() === [1101, 100, -1, 4, 99].join());
 // https://adventofcode.com/2019/day/5/input
 var day05input = [3,225,1,225,6,6,1100,1,238,225,104,0,101,14,135,224,101,-69,224,224,4,224,1002,223,8,223,101,3,224,224,1,224,223,223,102,90,169,224,1001,224,-4590,224,4,224,1002,223,8,223,1001,224,1,224,1,224,223,223,1102,90,45,224,1001,224,-4050,224,4,224,102,8,223,223,101,5,224,224,1,224,223,223,1001,144,32,224,101,-72,224,224,4,224,102,8,223,223,101,3,224,224,1,223,224,223,1102,36,93,225,1101,88,52,225,1002,102,38,224,101,-3534,224,224,4,224,102,8,223,223,101,4,224,224,1,223,224,223,1102,15,57,225,1102,55,49,225,1102,11,33,225,1101,56,40,225,1,131,105,224,101,-103,224,224,4,224,102,8,223,223,1001,224,2,224,1,224,223,223,1102,51,39,225,1101,45,90,225,2,173,139,224,101,-495,224,224,4,224,1002,223,8,223,1001,224,5,224,1,223,224,223,1101,68,86,224,1001,224,-154,224,4,224,102,8,223,223,1001,224,1,224,1,224,223,223,4,223,99,0,0,0,677,0,0,0,0,0,0,0,0,0,0,0,1105,0,99999,1105,227,247,1105,1,99999,1005,227,99999,1005,0,256,1105,1,99999,1106,227,99999,1106,0,265,1105,1,99999,1006,0,99999,1006,227,274,1105,1,99999,1105,1,280,1105,1,99999,1,225,225,225,1101,294,0,0,105,1,0,1105,1,99999,1106,0,300,1105,1,99999,1,225,225,225,1101,314,0,0,106,0,0,1105,1,99999,108,226,677,224,1002,223,2,223,1006,224,329,1001,223,1,223,1007,226,226,224,1002,223,2,223,1006,224,344,101,1,223,223,1008,226,226,224,102,2,223,223,1006,224,359,1001,223,1,223,107,226,677,224,1002,223,2,223,1005,224,374,101,1,223,223,1107,677,226,224,102,2,223,223,1006,224,389,101,1,223,223,108,677,677,224,102,2,223,223,1006,224,404,1001,223,1,223,1108,677,226,224,102,2,223,223,1005,224,419,101,1,223,223,1007,677,226,224,1002,223,2,223,1006,224,434,101,1,223,223,1107,226,226,224,1002,223,2,223,1006,224,449,101,1,223,223,8,677,226,224,102,2,223,223,1006,224,464,1001,223,1,223,1107,226,677,224,102,2,223,223,1005,224,479,1001,223,1,223,1007,677,677,224,102,2,223,223,1005,224,494,1001,223,1,223,1108,677,677,224,102,2,223,223,1006,224,509,101,1,223,223,1008,677,677,224,102,2,223,223,1005,224,524,1001,223,1,223,107,226,226,224,1002,223,2,223,1005,224,539,101,1,223,223,7,226,226,224,102,2,223,223,1005,224,554,101,1,223,223,1108,226,677,224,1002,223,2,223,1006,224,569,1001,223,1,223,107,677,677,224,102,2,223,223,1005,224,584,101,1,223,223,7,677,226,224,1002,223,2,223,1005,224,599,101,1,223,223,108,226,226,224,1002,223,2,223,1005,224,614,101,1,223,223,1008,677,226,224,1002,223,2,223,1005,224,629,1001,223,1,223,7,226,677,224,102,2,223,223,1005,224,644,101,1,223,223,8,677,677,224,102,2,223,223,1005,224,659,1001,223,1,223,8,226,677,224,102,2,223,223,1006,224,674,1001,223,1,223,4,223,99,226];
 run(day05input, 1);
+
+/*
+--- Part Two ---
+The air conditioner comes online! Its cold air feels good for a while, but then the TEST alarms start to go off. Since the air conditioner can't vent its heat anywhere but back into the spacecraft, it's actually making the air inside the ship warmer.
+
+Instead, you'll need to use the TEST to extend the thermal radiators. Fortunately, the diagnostic program (your puzzle input) is already equipped for this. Unfortunately, your Intcode computer is not.
+
+Your computer is only missing a few opcodes:
+
+Opcode 5 is jump-if-true: if the first parameter is non-zero, it sets the instruction pointer to the value from the second parameter. Otherwise, it does nothing.
+Opcode 6 is jump-if-false: if the first parameter is zero, it sets the instruction pointer to the value from the second parameter. Otherwise, it does nothing.
+Opcode 7 is less than: if the first parameter is less than the second parameter, it stores 1 in the position given by the third parameter. Otherwise, it stores 0.
+Opcode 8 is equals: if the first parameter is equal to the second parameter, it stores 1 in the position given by the third parameter. Otherwise, it stores 0.
+Like all instructions, these instructions need to support parameter modes as described above.
+
+Normally, after an instruction is finished, the instruction pointer increases by the number of values in that instruction. However, if the instruction modifies the instruction pointer, that value is used and the instruction pointer is not automatically increased.
+
+For example, here are several programs that take one input, compare it to the value 8, and then produce one output:
+
+3,9,8,9,10,9,4,9,99,-1,8 - Using position mode, consider whether the input is equal to 8; output 1 (if it is) or 0 (if it is not).
+3,9,7,9,10,9,4,9,99,-1,8 - Using position mode, consider whether the input is less than 8; output 1 (if it is) or 0 (if it is not).
+3,3,1108,-1,8,3,4,3,99 - Using immediate mode, consider whether the input is equal to 8; output 1 (if it is) or 0 (if it is not).
+3,3,1107,-1,8,3,4,3,99 - Using immediate mode, consider whether the input is less than 8; output 1 (if it is) or 0 (if it is not).
+Here are some jump tests that take an input, then output 0 if the input was zero or 1 if the input was non-zero:
+
+3,12,6,12,15,1,13,14,13,4,13,99,-1,0,1,9 (using position mode)
+3,3,1105,-1,9,1101,0,0,12,4,12,99,1 (using immediate mode)
+Here's a larger example:
+
+3,21,1008,21,8,20,1005,20,22,107,8,21,20,1006,20,31,
+1106,0,36,98,0,0,1002,21,125,20,4,20,1105,1,46,104,
+999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99
+The above example program uses an input instruction to ask for a single number. The program will then output 999 if the input value is below 8, output 1000 if the input value is equal to 8, or output 1001 if the input value is greater than 8.
+
+This time, when the TEST diagnostic program runs its input instruction to get the ID of the system to test, provide it 5, the ID for the ship's thermal radiator controller. This diagnostic test suite only outputs one number, the diagnostic code.
+
+What is the diagnostic code for system ID 5?
+*/
+
+function run2(original, inputValue) {
+  var program = original.slice();
+  let pc = 0;
+  while (true) {
+    let opcode = program[pc] % 100;
+    if (opcode === 99) {
+      return program;
+    }
+    let s = `${program[pc]}`.padStart(5, '0');
+    let paramMode1 = s[2];
+    let paramMode2 = s[1];
+    let paramMode3 = s[0];
+    var param1 = program[pc+1];
+    if (paramMode1 === '0') {
+      param1 = program[param1];
+    }
+    if (opcode === 1) { // add p3 = p1 + p2
+      var param2 = program[pc+2];
+      if (paramMode2 === '0') {
+        param2 = program[param2];
+      }
+      program[program[pc+3]] = param1 + param2;
+      pc += 4;
+    } else if (opcode === 2) { // mul p3 = p1 * p2
+      var param2 = program[pc+2];
+      if (paramMode2 === '0') {
+        param2 = program[param2];
+      }
+      program[program[pc+3]] = param1 * param2;
+      pc += 4;
+    } else if (opcode === 3) { // p1 = input
+      program[program[pc+1]] = inputValue;
+      pc += 2;
+    } else if (opcode === 4) { // output p1
+      console.log(param1);
+      pc += 2
+    } else if (opcode === 5) { // jump if true; if p1 then pc = p2
+      if (param1 !== 0) {
+        var param2 = program[pc+2];
+        if (paramMode2 === '0') {
+          param2 = program[param2];
+        }
+        pc = param2;
+      } else {
+        pc += 3;
+      }
+    } else if (opcode === 6) { // jump if false; if p1==0 then pc = p2
+      if (param1 === 0) {
+        var param2 = program[pc+2];
+        if (paramMode2 === '0') {
+          param2 = program[param2];
+        }
+        pc = param2;
+      } else {
+        pc += 3;
+      }
+    } else if (opcode === 7) { // less p3 = (p1 < p2) ? 1 : 0
+      var param2 = program[pc+2];
+      if (paramMode2 === '0') {
+        param2 = program[param2];
+      }
+      program[program[pc+3]] = (param1 < param2) ? 1 : 0;
+      pc += 4;
+    } else if (opcode === 8) { // eq p3 = (p1 == p2) ? 1 : 0
+      var param2 = program[pc+2];
+      if (paramMode2 === '0') {
+        param2 = program[param2];
+      }
+      program[program[pc+3]] = (param1 === param2) ? 1 : 0;
+      pc += 4;
+    } else {
+      return "illegal instruction " + opcode + " at pc " + pc + "\n" + program;
+    }
+  }
+}
+// eq 8 pos
+console.assert(run2([3,9,8,9,10,9,4,9,99,-1,8], 8)[9] === 1);
+console.assert(run2([3,9,8,9,10,9,4,9,99,-1,8], 0)[9] === 0);
+// lt 8 pos
+console.assert(run2([3,9,7,9,10,9,4,9,99,-1,8], 8)[9] === 0);
+console.assert(run2([3,9,7,9,10,9,4,9,99,-1,8], 7)[9] === 1);
+// eq 8 imm
+console.assert(run2([3,3,1108,-1,8,3,4,3,99], 8)[3] === 1);
+console.assert(run2([3,3,1108,-1,8,3,4,3,99], 7)[3] === 0);
+// lt 8 imm
+console.assert(run2([3,3,1107,-1,8,3,4,3,99], 8)[3] === 0);
+console.assert(run2([3,3,1107,-1,8,3,4,3,99], 7)[3] === 1);
+// zero?
+console.assert(run2([3,12,6,12,15,1,13,14,13,4,13,99,-1,0,1,9], 0)[13] === 0);
+console.assert(run2([3,12,6,12,15,1,13,14,13,4,13,99,-1,0,1,9], 88)[13] === 1);
+console.assert(run2([3,3,1105,-1,9,1101,0,0,12,4,12,99,1], 0)[12] === 0);
+console.assert(run2([3,3,1105,-1,9,1101,0,0,12,4,12,99,1], 88)[12] === 1);
+// 8 <=> --> 999 / 1000 / 1001
+console.assert(run2([3,21,1008,21,8,20,1005,20,22,107,8,21,20,1006,20,31,
+1106,0,36,98,0,0,1002,21,125,20,4,20,1105,1,46,104,
+999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99], 7)[20] === 0);
+console.assert(run2([3,21,1008,21,8,20,1005,20,22,107,8,21,20,1006,20,31,
+1106,0,36,98,0,0,1002,21,125,20,4,20,1105,1,46,104,
+999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99], 8)[20] === 1000);
+console.assert(run2([3,21,1008,21,8,20,1005,20,22,107,8,21,20,1006,20,31,
+1106,0,36,98,0,0,1002,21,125,20,4,20,1105,1,46,104,
+999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99], 9)[20] === 1001);
+
+console.log(run2(day05input, 5));
